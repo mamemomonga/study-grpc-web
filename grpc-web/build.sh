f679e9661bffaf65587ea441cc6bac7e0eb9e8fc
mamemomonga <mamemomonga@gmail.com>
2019-01-19 04:11:02 +0900

    もどした

--- grpc-web/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PATH="$BASEDIR/node_modules/.bin:$PATH"
-
-set -x
-webpack-cli --config=webpack1.config.js
-webpack-cli --config=webpack2.config.js
-
